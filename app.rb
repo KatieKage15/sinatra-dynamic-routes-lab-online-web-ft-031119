831fbd1f03e2854375a5844ce77f7f82dbb55f24
victorious-messaging-4309 <wildayk@gmail.com>
2019-04-24 11:13:30 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,11 +2,11 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-  get '/reversename/:name'
+  get '/reversename/:name' do 
     params[:name].reverse
   end
 
-  get '/square/:number' do 
+  get '/square/:number' do
     (params[:number].to_i ** 2).to_s
   end
 
